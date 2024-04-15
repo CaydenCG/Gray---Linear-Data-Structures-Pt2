72e2eb8a077a6940d8bb1438e07517e7157c8df5
Cayden <Caydencgray@gmail.com>
2024-04-15 09:04:09 -0400

    Add project files.

---
+++ Gray - Linear Data Structures Pt2/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public abstract class ArrayProcessor
+{
+    protected int[] Numbers;
+
+    public ArrayProcessor(int[] numbers)
+    {
+        Numbers = numbers;
+    }
+
+    public abstract void ProcessArray();
+}
+
+public class NumberCounter : ArrayProcessor
+{
+    public NumberCounter(int[] numbers) : base(numbers)
+    {
+    }
+
+    public override void ProcessArray()
+    {
+        Dictionary<int, int> numberCounts = new Dictionary<int, int>();
+
+        foreach (int num in Numbers)
+        {
+            if (numberCounts.ContainsKey(num))
+            {
+                numberCounts[num]++;
+            }
+            else
+            {
+                numberCounts[num] = 1;
+            }
+        }
+
+        var sortedKeys = numberCounts.Keys.OrderBy(k => k);
+
+        foreach (var key in sortedKeys)
+        {
+            Console.WriteLine($"Number {key} occurs {numberCounts[key]} time(s).");
+        }
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        try
+        {
+            Console.WriteLine("Enter the numbers separated by spaces:");
+            string input = Console.ReadLine();
+
+            int[] numbers = input.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                                  .Where(s => int.TryParse(s, out _))
+                                  .Select(int.Parse)
+                                  .ToArray();
+
+            if (numbers.Length == 0)
+            {
+                throw new Exception("No valid numbers were entered.");
+            }
+
+            ArrayProcessor arrayProcessor = new NumberCounter(numbers);
+            arrayProcessor.ProcessArray();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
+}
